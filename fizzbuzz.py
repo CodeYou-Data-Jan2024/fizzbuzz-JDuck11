2ade201cba9b81b29bfa6f6d7969f36566f8a34b
jduck11 <jjhowa02@icloud.com>
2024-03-26 22:34:12 -0400

    fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,21 @@
-# add your code here
+# fizzbuzz printing number 1-100
+
+for num in range (1,101):
+
+
+#number is multiple of 3 print "fizz"
+#number is multiple of 5 print "buzz"
+#number is multiple of both 5 & 3 print "fizzbuzz"
+
+    if num % 3 == 0 and num % 5 == 0:
+        print ("FizzBuzz")
+        continue
+    elif num % 3 == 0:
+        print ("Fizz")
+        continue
+    elif num % 5 == 0:
+        print("Buzz")
+        continue
+
+    print(num)
 
